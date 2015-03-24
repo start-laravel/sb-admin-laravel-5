ccd6fb21f40e7f4b3005beb1d3e937ac260fe98f
sankhadeep <sankhadeep@sahusoft.com>
2015-03-24 12:44:16 +0530

    vendor folder removed

--- gulpfile.js
+++ gulpfile.js
@@ -13,28 +13,22 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.less('app.less');
-    mix.copy('bower_components/jquery/dist/jquery.js', 'resources/js/jquery.js');
-    mix.copy('bower_components/Chart.js/Chart.js', 'resources/js/Chart.js');
-    mix.copy('bower_components/metisMenu/dist/metisMenu.js', 'resources/js/metisMenu.js');
-    mix.copy('bower_components/bootstrap/dist/js/bootstrap.js', 'resources/js/bootstrap.js');
-    mix.copy('bower_components/font-awesome/css/font-awesome.css', 'resources/css/font-awesome.css');
-    mix.copy('bower_components/bootstrap/dist/css/bootstrap.css', 'resources/css/bootstrap.css');
     mix.copy('bower_components/bootstrap/dist/fonts', 'public/assets/fonts');
    	mix.copy('bower_components/font-awesome/fonts', 'public/assets/fonts');
    	mix.styles([
-        "bootstrap.css",
-        "font-awesome.css",
-        "sb-admin-2.css",
-        "timeline.css"
-    ], 'public/assets/stylesheets/styles.css');
+        'bower_components/bootstrap/dist/css/bootstrap.css',
+        'bower_components/font-awesome/css/font-awesome.css',
+        'resources/css/sb-admin-2.css',
+        'resources/css/timeline.css'
+    ], 'public/assets/stylesheets/styles.css', './');
     mix.scripts([
-        "jquery.js",
-        "bootstrap.js",
-        "Chart.js",
-        "metisMenu.js",
-        "sb-admin-2.js",
-        "frontend.js"
-    ], 'public/assets/scripts/frontend.js');
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/bootstrap/dist/js/bootstrap.js',
+        'bower_components/Chart.js/Chart.js',
+        'bower_components/metisMenu/dist/metisMenu.js',
+        'resources/js/sb-admin-2.js',
+        'resources/js/frontend.js'
+    ], 'public/assets/scripts/frontend.js', './');
 });
 
 
